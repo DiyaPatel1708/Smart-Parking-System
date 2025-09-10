507633c7a7c0005c969e72e5664c751bd34a5b41
Prapti-1312 <chotaliyaprapti1312@gmail.com>
2025-09-10 23:57:39 +0530

    Add parking spot and garage class

---
+++ sps_1.cpp
@@ -0,0 +1,123 @@
+#include<iostream>
+#include<string>
+#include<ctime>
+#include<vector>
+#include<fstream>
+using namespace std;
+
+enum class vehicleType{car,motorcycle,EV,disabled};
+enum class spotStatus {free,reserved,occupied,maintenance};
+enum class parkingSpotType {
+    standard,    
+    premium,     
+    ev_charging, 
+    disabled,    
+    motorcycle  
+};
+class parkingGarage {
+private:
+    string name;
+    string location;
+    static int totalGarages;
+public: 
+    parkingGarage(const string &name, const string &location){
+        this->name=name;
+        this->location=location;
+        totalGarages++;
+    }
+    int getTotalGarages(){
+        return totalGarages; 
+    }
+    void displayInfo() const{
+        cout<<"Garage: "<<name<<" at "<<location<<"\n";
+    }
+    ~parkingGarage(){
+        totalGarages--;
+    }
+};
+int parkingGarage::totalGarages=0;
+class parkingSpot{
+private:
+    string spotID;
+    spotStatus status;
+    vehicleType vehicle;
+    parkingSpotType type;
+    time_t reservationTime;
+public:
+    parkingSpot(const string& id, vehicleType type);
+    string getSpotId() const{ 
+        return spotID; 
+    }
+    spotStatus getStatus() const{ 
+        return status; 
+    }
+    bool isVehicleCompatibleWithSpot(vehicleType vehicle,parkingSpotType spot){
+        switch(spot){
+            case parkingSpotType::standard:
+                return (vehicle == vehicleType::car || vehicle == vehicleType::disabled);
+            case parkingSpotType::premium:
+                return (vehicle == vehicleType::car);
+            case parkingSpotType::ev_charging:
+                return (vehicle == vehicleType::EV);
+            case parkingSpotType::disabled:
+                return (vehicle == vehicleType::disabled);
+            case parkingSpotType::motorcycle:
+                return (vehicle == vehicleType::motorcycle);
+            default:
+                return false;
+        }
+    }
+    bool parkingSpot::reserve(vehicleType vehicle) {  
+        if (status != spotStatus::free) {
+            cout << "Spot " << spotID << " is not free!\n";
+            return false; 
+        }
+        if (vehicle != suitableFor){
+            cout<<"Vehicle type not suitable for spot "<<spotID<<"!\n";
+            return false; 
+        }
+        status=spotStatus::reserved;
+        cout<<"Spot "<< spotID<<" reserved successfully.\n";
+        return true;  
+    }
+    char getSpotSymbol(parkingSpotType type) {
+        switch (type){
+            case parkingSpotType::standard: 
+            return 'S';
+            case parkingSpotType::premium: 
+            return 'P';
+            case parkingSpotType::ev_charging: 
+            return 'E';
+            case parkingSpotType::disabled: 
+            return 'D';
+            case parkingSpotType::motorcycle:
+            return 'M';
+            default: return '?';
+        }
+    } 
+    bool reserve(vehicleType vehicle,time_t reservedUntil){
+        if(!reserve(vehicle)){ 
+            return false;
+        }
+        reservationTime=reservedUntil;
+        cout<<"Reservation for spot "<<spotID<<" is valid until "<<ctime(&reservedUntil);
+        return true;
+    }
+    bool release(){
+        if (status==spotStatus::free) {
+            return false; 
+        }
+        status=spotStatus::free;
+        reservationTime=0;
+        cout<<"Spot "<<spotID<<" has been released.\n";
+        return true;
+    }
+    friend void checkSpotDetails(const parkingSpot &spot){
+        cout<<"[Friend Function] Spot ID: "<<spot.spotID<<", Status: ";
+        cout<<"\n";
+    }
+};
+int main(){
+
+    return 0;
+}
